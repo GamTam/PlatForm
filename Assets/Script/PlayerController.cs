297d0624bc3db5b453b713893b55490f32bff5aa
Mabel <59884023+GamTam@users.noreply.github.com>
2026-09-10 19:59:07 -0400

    Professional Gaming at hand

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,36 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float _moveSpeed = 6f;
+    [SerializeField] private float _jumpStrength = 10f;
+    
+    private Rigidbody2D _rb;
+    
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float move = Input.GetAxis("Horizontal");
+
+        _rb.linearVelocity = new Vector2(move * _moveSpeed, _rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpStrength);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
